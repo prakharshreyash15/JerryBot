55a8d2b48eb8dd0bda62f0248ad609c648bbfc15
Matthew Gee <geem2014@gmail.com>
2017-11-14 03:31:17 -0800

    string format changes, comments

--- modules/dunce.py
+++ modules/dunce.py
@@ -3,30 +3,32 @@ from time import sleep
 
 
 def duncecap(args, perms={}):
-    print("In duncecap")
+    
+    # Get name and uid of person
     name = " ".join(args)
     uid = perms[FN_GET_UID](name)
 
-    print(perms[MESSAGE_THREADID])
-    print(perms[THREAD_NICKNAMES])
+    # Get all nicknames in thread
     nicknames = perms[THREAD_NICKNAMES](perms[MESSAGE_THREADID])
-
+    
+    # Check that no one is currently the dunce
     for k, v in nicknames.items():
         if v == "Town Dunce":
             name = perms[FN_GET_NAME](k)
-            perms[FN_SEND_MESSAGE](f"{name} is already the Town Dunce", perms[MESSAGE_THREADID])
+            perms[FN_SEND_MESSAGE]("%s is already the Town Dunce" % (name), perms[MESSAGE_THREADID])
             return ""
-
-    print("names")
+    # Store old nickname
     old_name = name
     if uid in nicknames:
         old_name = nicknames[uid]
     print(old_name, uid, perms[MESSAGE_THREADID])
 
+    # Change to duncecap, send message
     perms[FN_CHANGE_NICKNAME]("Town Dunce", uid, perms[MESSAGE_THREADID])
 
-    perms[FN_SEND_MESSAGE](f"{name} is now the Town Dunce", perms[MESSAGE_THREADID])
+    perms[FN_SEND_MESSAGE]("%s is now the Town Dunce" % (name), perms[MESSAGE_THREADID])
 
+    # Wait 10 minutes, then change their name back
     sleep(600)
     perms[FN_CHANGE_NICKNAME](old_name, uid, perms[MESSAGE_THREADID])
-    perms[FN_SEND_MESSAGE](f"{name} is no longer the Town Dunce", perms[MESSAGE_THREADID])
+    perms[FN_SEND_MESSAGE]("%s is no longer the Town Dunce" % (name), perms[MESSAGE_THREADID])
