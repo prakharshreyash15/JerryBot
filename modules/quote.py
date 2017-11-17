1d37ec4bbd770937a1916a81de8030be3389e85a
Matthew Gee <geem2014@gmail.com>
2017-11-16 22:25:18 -0800

    rupi kaur memes with |

--- modules/quote.py
+++ modules/quote.py
@@ -8,16 +8,13 @@ from modules.permissions import *
 from typing import Text, Optional
 
 
-def format_image(message: Text, author: Optional[Text]):
-    if author is None:
-        author is "someone stupid"
+def format_image(message: Text):
     r = requests.get("https://picsum.photos/800/600/?random", stream=True)
     if r.status_code == 200:
         img = Image.open(BytesIO(r.content))
-        formatted_text = "\n".join(textwrap.wrap(message, 24)) + ("\n       -%s" %(author))
         font = ImageFont.truetype("modules/fonts/font.ttf", 72)
         draw = ImageDraw.Draw(img)
-        draw.text((100, 100), formatted_text, (255, 255, 255), font=font)
+        draw.text((100, 100), message, (255, 255, 255), font=font)
         img.save("image.jpg")
 
 
@@ -25,12 +22,16 @@ def imagequote(args=[], perms={}):
     words = " ".join(args)
     sections = words.split("-")
     message = sections[0].strip()
-    message.replace("|", "\n")
+    if "|" in message:   
+        message = message.replace("|", "\n")
+    else:
+        message = "\n".join(textwrap.wrap(message, 24))
     author = None
     try:
         author = sections[1].strip()
     except IndexError:
-        pass
+        author = "someone stupid"
 
-    format_image(message, author)
+    message = message + ("\n       -%s" %(author))
+    format_image(message)
     perms[FN_SEND_IMAGE]("image.jpg", perms[MESSAGE_THREADID])
